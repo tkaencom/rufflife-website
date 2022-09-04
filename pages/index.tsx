ca002161f7eef54b7bbc9b8a1156fc3635bce488
abdullahmafuz2020 <abdullah.mohammed@dubizzle.com>
2022-09-04 18:22:47 +0400

    add official favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,8 +13,14 @@ const Home: NextPage = () => {
     <>
       <Head>
         <title>Welcome to Rufflife</title>
-        <meta name="description" content="Welcome to Rufflife" />
-        <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="Our comprehensive solution includes delivery of Pet care, Food, Grooming & Health service as a packaged experience on a scheduled basis for the well-being of your pet." />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="manifest" href="/site.webmanifest"/>
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
+        <meta name="msapplication-TileColor" content="#da532c"/>
+        <meta name="theme-color" content="#ffffff"/>
       </Head>
       <Navbar/>
       <div>
